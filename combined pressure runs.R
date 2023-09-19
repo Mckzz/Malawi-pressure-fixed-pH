b3d8ff714cca71c847f90e12e4cd920da53af58d
Evan McKenzie <evan.mcken@gmail.com>
2023-09-19 12:06:08 -0700

    pH 6 finished (maybe some ironing left to do), with some potential modelling. probably just loess for now though

--- combined pressure runs.R
+++ combined pressure runs.R
@@ -23,20 +23,121 @@ larvae.df <- read_delim(list_of_results,
   mutate(" " = NULL) %>%
   mutate(larva = substr(larva, 1, nchar(larva)-4)) %>% #drop file extension
   group_by(larva, sac) %>%
-  mutate(psi = (row_number() *30) -30)
+  mutate(psi = (row_number() * 30) - 30) %>% # x axis starting from zero
+  mutate(area_set_0psi = Area - Area[1]) %>% # still abs size, but starting from o psi
+  mutate(pct.area = ((Area - Area[1]) / Area[1]) * 100)
+
 
 print(larvae.df)
 
-
+# absolute sizez
 ggplot(data = larvae.df, 
        aes(x= psi,
            group = interaction(larva, sac),
            colour = larva), na.rm = F) +
+#  geom_point(aes(y= Area, size = 2)) +
   geom_point(aes(y= Area)) +
-  geom_line(aes(y= Area)) 
+  geom_line(aes(y= Area))
+
+
+# difference from 0 psi
+ggplot(data = larvae.df, 
+       aes(x= psi,
+           group = interaction(larva, sac),
+           colour = larva), na.rm = F) +
+#  geom_point(aes(y= area_set_0psi, size = 2)) +
+  geom_point(aes(y= area_set_0psi)) +
+  geom_line(aes(y= area_set_0psi))
+
+# % change
+ggplot(data = larvae.df, 
+       aes(x= psi,
+           group = interaction(larva, sac),
+           colour = larva), na.rm = F) +
+  geom_point(aes(y= pct.area), size = 2) 
+  #geom_point(aes(y= pct.area)) +
+  #geom_line(aes(y= pct.area))
+
+###########   looking at/ comparing individual larvae   ##########
+
+larva_choose <- larvae.df %>%
+  filter(larva == "larva 11" | larva == "larva 3")
+print(larva_choose)
+
+# absolute sizez
+ggplot(data = larva_choose, 
+       aes(x= psi,
+           group = interaction(larva, sac),
+           colour = larva), na.rm = F) +
+  geom_point(aes(y= Area, size = 1.5)) +
+  geom_line(aes(y= Area))
+
+
+# difference from 0 psi
+ggplot(data = larva_choose, 
+       aes(x= psi,
+           group = interaction(larva, sac),
+           colour = larva), na.rm = F) +
+  geom_point(aes(y= area_set_0psi)) +
+  geom_line(aes(y= area_set_0psi))
+
+
+###############################################################
+install.packages("ggformula")
+library(ggformula)
+
+ggplot(larvae.df, aes(y = pct.area, x = psi)) + 
+  geom_point(size = 3, col = "firebrick") + 
+  geom_spline(col = "black", df = 4) +
+  theme_minimal()
+
+log_larvae.df <- larvae.df %>%
+  mutate(kpa_sealv = (psi * 6.89476) + 101.325) %>%
+  mutate(log_kpa_sealv = log(kpa_sealv)) %>%
+  mutate(log_area = log(Area)) %>%
+  mutate(pct100.area = pct.area + 100) %>%
+  mutate(log_pct100.area = log(pct100.area)) %>%
+  select(-area_set_0psi) %>%
+  select(larva, sac, type, 
+         psi, kpa_sealv, log_kpa_sealv, 
+         Area, log_area, pct.area, pct100.area, log_pct100.area)
+
+print(log_larvae.df)
+
+ggplot(log_larvae.df, aes(y = pct100.area, x = kpa_sealv)) + 
+  geom_point(size = 3, col = "firebrick") + 
+  #geom_spline(col = "black", df = 4) +
+  geom_smooth(method = "loess", se = T)
+  theme_minimal()
+
+
+#try a Michaelis-Menten curve/ model
+mich_men <- nls(Area ~ a*kpa_sealv / ( b+kpa_sealv), 
+                  data = log_larvae.df, list(a = 1, b = 1))
+#plot
+ggplot(log_larvae.df, aes(y = pct100.area, x = kpa_sealv)) + 
+  geom_smooth(method = "nls", method.args = 
+                list(formula = y ~ a * x / (b + x), start = list(a = 1, b = 1)), 
+              data = log_larvae.df, se = FALSE, col = "black") + 
+  geom_point(size = 3, col = "firebrick") +
+  theme_classic()
 
 
 
+# various linear models
+mod1 <- lm(Area ~ psi, larvae.df)
+mod2 <- lm(Area ~ psi + type, larvae.df)
+mod3 <- lm(Area ~ psi * type, larvae.df)
+mod4 <- lm(Area ~ (psi + I(psi^2)) * type, larvae.df)
+mod5 <- lm(Area ~ (psi + I(psi^2) + I(psi^3)) * type, larvae.df)
 
+plot(mod2)
+
+ggplot(larvae.df, aes(y = pct.area, x = psi)) +
+  geom_point(size = 2, col = "red") +
+  geom_smooth(method = "lm", se = FALSE,
+              formula = y ~ (x + I((x^-2)))) +
+  theme(aspect.ratio = 0.80) +
+  theme_classic()
 
 
--- deal with multiple sacs per image, images per larva.R
+++ deal with multiple sacs per image, images per larva.R
@@ -7,17 +7,19 @@ setwd("~/student_documents/UBC/Research/Malawi/data/sac pressure, fixed pH")
 
 
 setwd("./sacs pH 6 then pressure")
-setwd("./area results, thresh calc")
+#setwd("./area results, thresh calc")
+
 
 #########       for multi sac images: make sac and type columns manually
-larva_5 <- read_csv("~/student_documents/UBC/Research/Malawi/data/sac pressure, fixed pH/sacs pH 6 then pressure/larva 5 raw.csv") %>%
+larva_12 <- read_csv("~/student_documents/UBC/Research/Malawi/data/sac pressure, fixed pH/sacs pH 6 then pressure/larva 12, raw.csv") %>%
   rename(" " = ...1) %>% #so that the output can be fed into the larger data frame
   arrange(sac) # so the psi x axis can be repeated over it in the other script
-print(larva_5)
+print(larva_12)
 
 # send combined csv to folder where main script reads from
-write_csv(larva_5,
-          "~/student_documents/UBC/Research/Malawi/data/sac pressure, fixed pH/sacs pH 6 then pressure/area results, thresh calc/larva 5.csv")
+write_csv(larva_12,
+          "~/student_documents/UBC/Research/Malawi/data/sac pressure, fixed pH/sacs pH 6 then pressure/area results, thresh calc/larva 12.csv")
+
 
 
 ########     when larva is over two image sets     #######
@@ -25,20 +27,23 @@ write_csv(larva_5,
 # bring all sacs from one larva into a single df, arranged by sac 
 # so that the psi x axis can still be created along with the larger df
 #########       after manually assigning sac number and type       #############
-larva_3a <- read_csv("~/student_documents/UBC/Research/Malawi/data/sac pressure, fixed pH/sacs pH 6 then pressure/3/larva 3a (still needs sacs separated, data wrangle).csv") %>%
+larva_8a <- read_csv("~/student_documents/UBC/Research/Malawi/data/sac pressure, fixed pH/sacs pH 6 then pressure/larva 8a, v2.csv") %>%
   rename(" " = ...1) %>% #so that the output can be fed into the larger data frame
   arrange(sac)
-print(larva_3b)
+print(larva_8a)
 
-larva_3b <- read_csv("~/student_documents/UBC/Research/Malawi/data/sac pressure, fixed pH/sacs pH 6 then pressure/3/larva 3b.csv") %>%
+larva_8b <- read_csv("~/student_documents/UBC/Research/Malawi/data/sac pressure, fixed pH/sacs pH 6 then pressure/larva 8b, v2.csv") %>%
   rename(" " = ...1) %>% #so that the output can be fed into the larger data frame
   arrange(sac)
-print(larva_3b)
+print(larva_8b)
 
 # combine to one df for larva
-larva_3 <- rbind(larva_3a, larva_3b)
-print(larva_3)
+larva_8 <- rbind(larva_8a, larva_8b)
+print(larva_8)
+
+#rm(larva_8)
+
 
 # send combined csv to folder where main script reads from
-write_csv(larva_3,
-           "~/student_documents/UBC/Research/Malawi/data/sac pressure, fixed pH/sacs pH 6 then pressure/area results, thresh calc/larva 3.csv")
+write_csv(larva_8,
+           "~/student_documents/UBC/Research/Malawi/data/sac pressure, fixed pH/sacs pH 6 then pressure/area results, thresh calc/larva 8.csv")
