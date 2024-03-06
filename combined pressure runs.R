d8f395437f8e4669c3cec62ed0aa3f45b025064f
Evan McKenzie <evan.mcken@gmail.com>
2024-03-06 10:35:47 -0800

    added in kpa. still need lengths at start for stress strain, and negative work?

--- combined pressure runs.R
+++ combined pressure runs.R
@@ -2,11 +2,15 @@ library(tidyverse)
 library(ggplot2)
 library(readr)
 
-setwd("~/student_documents/UBC/Research/Malawi/data/sac pressure, fixed pH")
+# setwd("~/student_documents/UBC/Research/Malawi/data/sac pressure, fixed pH")
+# setwd("./sacs pH 6 then pressure")
+# setwd("./area results, thresh calc")
 
+#setwd("~/student_documents/UBC/Research/Malawi\\KenyaData\\DinoLite\\pH6_sac_pressurization/area results, thresh calc")
 
-setwd("./sacs pH 8 then pressure")
-setwd("./area results, thresh calc")
+#setwd("~/student_documents/UBC/Research/Malawi\\data\\sac pressure, fixed pH\\sacs pH 6 then pressure/area results, thresh calc")
+ 
+setwd("~/student_documents/UBC/Research/Malawi\\americanus comparison\\pH6/area results, thresh calc")
 
 
 # the list of file names will provide the files for r to read in, as well as 
@@ -15,6 +19,7 @@ list_of_results <- # list of file names
   list.files(pattern = "\\.csv$",
              full.names = F) #leave out folder path
 print(list_of_results)
+
 #rm(list_of_results)
 
 # read in files, modify text in larva column, add psi column for x axis
@@ -22,17 +27,23 @@ larvae.df <- read_delim(list_of_results,
                       delim = ",",
                       id = "larva") %>%
   mutate(" " = NULL) %>%
+  mutate(location = "Kenya") %>%
   mutate(larva = substr(larva, 1, nchar(larva)-4)) %>% #drop file extension
   group_by(larva, sac) %>%
-  mutate(psi = (row_number() * 30) - 30) %>% # x axis starting from zero
+  mutate(psi = (row_number() * 30) - 30) %>% # x axis starting from zero [[[((( FOR MALAWI DATA )))]]]
+  #mutate(psi = (row_number() * 10) - 10) %>% # x axis starting from zero [[[((( FOR KENYA DATA )))]]]
+  #slice(-1) %>% # FOR AMERICANUS removes 1st obsv of group (still to figure out showing 0 - 01h change)
   mutate(area_set_0psi = Area - Area[1]) %>% # still abs size, but starting from o psi
+  mutate(delta_frac.area = abs((Area - Area[1]) / Area[1])) %>%
   mutate(pct.area = ((Area - Area[1]) / Area[1]) * 100) %>%
   ungroup() %>%
   group_by(psi) %>%
   mutate(mean_pct_area = mean(pct.area)) %>% # mean for all sacs at a psi
-  mutate(sd_pct_area = sd(pct.area))
+  mutate(sd_pct_area = sd(pct.area)) 
 
 print(larvae.df)
+
+#larvae.df <- larvae.df %>% 
 
 # % change
 ggplot(data = larvae.df, 
@@ -44,17 +55,18 @@ ggplot(data = larvae.df,
   geom_point(aes(y= mean_pct_area), size = 3, colour = "black") +
   geom_line(aes(y= mean_pct_area), colour = "black")
 
-
-
 # absolute sizez
 ggplot(data = larvae.df, 
        aes(x= psi,
            group = interaction(larva, sac),
            colour = larva), na.rm = F) +
-#  geom_point(aes(y= Area, size = 2)) +
+  geom_smooth(method = 'lm', 
+              formula = y ~ splines::bs(x, df = 4, knots = 200),
+              inherit.aes = F, 
+              aes(x = psi, y= Area), 
+              color = '#555555') +
   geom_point(aes(y= Area)) +
   geom_line(aes(y= Area))
-
 
 # difference from 0 psi
 ggplot(data = larvae.df, 
@@ -67,18 +79,88 @@ ggplot(data = larvae.df,
 
 
 ######################   making the file for a given pH   #########################
+getwd()
+
 
 write_csv(larvae.df,
-          "~/student_documents/UBC/Research/Malawi/data/sac pressure, fixed pH/larvae_pH8.csv")
-
+          "../../data frames by pH/larvae_pH6.csv")
 
 
 ###################################################################################
 
+## combine the three pH crushes
+setwd("../../data frames by pH")
+pH_crush_files <- # list of file names
+  list.files(pattern = "\\.csv$",
+             full.names = F) #leave out folder path
+print(pH_crush_files)
+
+pH_crushes.BC <- read_delim(pH_crush_files, ###  rename for location   ###
+                         delim = ",",
+                         id = NULL)
+
+pH_crushes.BC <- pH_crushes.BC %>%  ###  rename for location   ###
+  ungroup() %>%
+  group_by(pH, psi) %>%
+  mutate(mean_area_by_pH = mean(Area)) %>%
+  mutate(mean_set0 = mean(area_set_0psi)) # mean for all sacs at a psi
+
+# absolute sizez
+ggplot(data = filter(pH_crushes, pH == "6"),
+         #pH_crushes,
+       aes(x= psi,
+           group = interaction(pH, larva, sac),
+           #group = as_factor(pH),
+           colour = as_factor(pH)), na.rm = F) +
+  geom_smooth(data = filter(pH_crushes, pH == 6), method = 'lm',
+              formula = y ~ splines::bs(x, df = 3, knots = 120),
+              inherit.aes = F,
+              aes(x = psi,
+                  y = Area,
+                  colour = as_factor(pH),
+                  #colour = "black", # for when displayed separately
+                  group = pH)) +
+  geom_smooth(data = filter(pH_crushes, pH == 7), method = 'lm',
+              formula = y ~ splines::bs(x, df = 3, knots = 300),
+              inherit.aes = F,
+              aes(x = psi,
+                  y = Area,
+                  colour = as_factor(pH),
+                  #colour = "black", # for when displayed separately
+                  group = pH)) +
+  geom_smooth(data = filter(pH_crushes, pH == 8), method = 'lm',
+              formula = y ~ splines::bs(x, df = 3, knots = 20),
+              inherit.aes = F,
+              aes(x = psi,
+                  y = Area,
+                  colour = as_factor(pH),
+                  #colour = "black", # for when displayed separately
+                  group = pH)) +
+  # geom_point(aes(y= Area), alpha = 0.2) +
+  # geom_line(aes(y= Area)) +
+  # ylim(0.1, 0.4) + # scale for Kenya sacs
+  # xlim(0, 750) + # scale for Kenya sacs
+  # ylim(0.05, 0.25) + # scale for Malawi sacs
+  # xlim(0, 850) + # scale for Malawi sacs
+  theme_classic() +
+  theme(legend.position = c(0.8, 0.8)) +
+  #theme(legend.position = "none") +
+  theme(axis.ticks.length = unit(-1, "mm")) 
+
+ggsave("three_splines_abs.pdf", 
+       units = c("cm"), 
+       width = 6, height = 6, 
+       path = "../")
+
+ #####################################################################################
+
+
+
+
 ###########   looking at/ comparing individual larvae   ##########
 
 larva_choose <- larvae.df %>%
-  filter(larva == "larva 9" | larva == "larva 10")
+  filter(larva == "larva 11" | larva == "larva 12")
 print(larva_choose)
 
 # absolute sizez
--- deal with multiple sacs per image, images per larva.R
+++ deal with multiple sacs per image, images per larva.R
@@ -3,26 +3,33 @@ library(ggplot2)
 library(readr)
 
 
-setwd("~/student_documents/UBC/Research/Malawi/data/sac pressure, fixed pH")
+# setwd("~/student_documents/UBC/Research/Malawi/data/sac pressure, fixed pH")
+# 
+# 
+# setwd("./sacs pH 6 then pressure")
+# #setwd("./area results, thresh calc")
 
-
-setwd("./sacs pH 7 then pressure")
-#setwd("./area results, thresh calc")
+setwd("~/student_documents/UBC/Research/Malawi\\americanus comparison\\pH8")
 
 
 #########       for multi sac images: make sac and type columns manually
-larva_12 <- read_csv(
-  "~/student_documents/UBC/Research/Malawi/data/sac pressure, fixed pH/sacs pH 7 then pressure/larva 12, raw.csv") %>%
+larva_8 <- read_csv(
+  "larva 8.csv") %>%
   rename(" " = ...1) %>% #so that the output can be fed into the larger data frame
   arrange(sac) %>% # so the psi x axis can be repeated over it in the other script
-  mutate(pH = 7) %>%
-  print()
+  group_by(sac) %>%
+  mutate(pH = 8) %>%
+  mutate(psi = c(0, 0, 10, 20,
+                 seq(25, length(sac) * 5, by = 5))) %>% # FOR AMERICANUS x axis changing sequence
+  print() %>%
+  write_csv("./area results, thresh calc/larva 8.csv") # send combined csv to folder where main script reads from
 
+# psi_scale <- c(seq(0, ((length(larva_3$sac) + 2) *5), by = 5))
+# view(psi_scale)
 
 # send combined csv to folder where main script reads from
-write_csv(larva_1,
-          "~/student_documents/UBC/Research/Malawi/data/sac pressure, fixed pH/sacs pH 7 then pressure/area results, thresh calc/larva 1.csv")
-
+# write_csv(larva_1,
+#           "./area results, thresh calc/larva 1.csv")
 
 
 ########     when larva is over two image sets     #######
@@ -30,19 +37,22 @@ write_csv(larva_1,
 # bring all sacs from one larva into a single df, arranged by sac 
 # so that the psi x axis can still be created along with the larger df
 #########       after manually assigning sac number and type       #############
-larva_3a <- read_csv("~/student_documents/UBC/Research/Malawi/data/sac pressure, fixed pH/sacs pH 6 then pressure/larva 3a, raw, v2.csv") %>%
+larva_10a <- read_csv("larva 10a.csv") %>%
   rename(" " = ...1) %>% #so that the output can be fed into the larger data frame
   arrange(sac)
-print(larva_3a)
+print(larva_10a)
 
-larva_3b <- read_csv("~/student_documents/UBC/Research/Malawi/data/sac pressure, fixed pH/sacs pH 6 then pressure/larva 3b, raw, v2.csv") %>%
+larva_10b <- read_csv("larva 10b.csv") %>%
   rename(" " = ...1) %>% #so that the output can be fed into the larger data frame
   arrange(sac)
-print(larva_3b)
+print(larva_10b)
 
 # combine to one df for larva
-larva_3 <- rbind(larva_3a, larva_3b) %>%
-  mutate(pH = 6) %>%
+larva_10 <- rbind(larva_10a, larva_10b) %>%
+  group_by(sac) %>%
+  mutate(pH = 8) %>%
+  mutate(psi = c(0, 0, 10, 20,
+                 seq(25, length(sac) * 5, by = 5))) %>% # FOR AMERICANUS x axis changing sequence
   print()
 
 
@@ -50,5 +60,5 @@ larva_3 <- rbind(larva_3a, larva_3b) %>%
 
 
 # send combined csv to folder where main script reads from
-write_csv(larva_3,
-           "~/student_documents/UBC/Research/Malawi/data/sac pressure, fixed pH/sacs pH 8 then pressure/area results, thresh calc/larva 3.csv")
+write_csv(larva_10,
+           "./area results, thresh calc/larva 10.csv")
